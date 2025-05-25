726da85674e6c971bbf79ff4a0470d2ee6f8ee22
KachVeV <kachevnikov.dev@gmail.com>
2025-05-25 23:11:54 +0300

    Changed serialization to inline reified function

--- src/main/kotlin/dev/zorinov/blockbench/service/ModelParser.kt
+++ src/main/kotlin/dev/zorinov/blockbench/service/ModelParser.kt
@@ -7,7 +7,7 @@ import java.io.File
 class ModelParser(val json: Json = Json { ignoreUnknownKeys = true }) {
     fun parse(file: File): BlockbenchModel {
         return file.readText().let { text ->
-            json.decodeFromString(BlockbenchModel.serializer(), text)
+            json.decodeFromString<BlockbenchModel>(text)
         }
     }
 }

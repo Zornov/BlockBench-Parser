7a8ad3ad2afb37cfb17fabbae8325cc1f9d72fae
KachVeV <kachevnikov.dev@gmail.com>
2025-05-25 23:48:54 +0300

    Publication implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.1.21"
     kotlin("plugin.serialization") version "2.1.21"
+    `maven-publish`
 }
 
-group = "dev.zorinov"
+group = "dev.zorinov.blockbench"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -23,4 +24,40 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("parser") {
+            from(components["java"])
+            artifactId = "parser"
+            groupId    = project.group.toString()
+            version    = project.version.toString()
+
+            pom {
+                name.set("parser")
+                description.set("Parser for models")
+                url.set("https://github.com/Zornoov/BlockBench-Parser")
+                developers {
+                    developer {
+                        id.set("Zornoov")
+                        name.set("Sasha Zornoov")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+
+        maven {
+            name = "GitHubPackages"
+            url  = uri("https://maven.pkg.github.com/Zornoov/BlockBench-Parser")
+            credentials {
+                username = findProperty("gpr.user") as String
+                password = findProperty("gpr.key")  as String
+            }
+            mavenContent { snapshotsOnly() }
+        }
+    }
 }
